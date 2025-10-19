44f7bf535d5ba59b502c5492a279d28ca99fafc0
Valentina <zenit@Valentinas-MacBook-Air.local>
2025-10-19 07:18:38 -0500

    redesign of game class

--- src/game.c
+++ src/game.c
@@ -1,7 +1,195 @@
 // game.c
 
-// new branch test
-
 #include <stdio.h>
 
 #include "game.h"
+
+game game_create(
+    char* window_title,
+    int window_width,
+    int window_height,
+    int grid_width,
+    int grid_height,
+    int move_interval
+) 
+{
+
+    game game;
+
+    rand_seed();
+
+    game.snake = snake_create(rand_int(0, grid_width - 1), rand_int(0, grid_height - 1));
+
+    game.apple_x = rand_int(0, grid_width - 1);
+    game.apple_y = rand_int(0, grid_height - 1);
+
+    game.input = input_create();
+    game.move_interval = move_interval;
+
+    renderer_create(&game.renderer, window_title, window_width, window_height);
+
+    game.window_width = window_width;
+    game.window_height = window_height;
+
+    game.grid_width = grid_width;
+    game.grid_height = grid_height;
+
+    game.running = 1;
+    game.paused = 0;
+
+    return game;
+
+}
+
+void game_run(game* game) {
+
+    if (game == NULL) return;
+
+    Uint32 last_move_time = SDL_GetTicks();
+
+    while (game->running) {
+        input_read(&game->input, &game->running);
+        _game_update(game, &last_move_time);
+        _game_render(game, 0, 255, 0, 0);
+    }
+
+    return;
+
+}
+
+void game_delete(game* game) {
+
+    if (game == NULL) return;
+
+    snake_delete(&game->snake);
+    renderer_delete(&game->renderer);
+
+    return;
+
+}
+
+void _game_restart(game* game) {
+
+    if (game == NULL) return;
+
+    snake_delete(&game->snake);
+
+    game->snake = snake_create(rand_int(0, game->grid_width - 1), rand_int(0, game->grid_height - 1));
+
+    game->apple_x = rand_int(0, game->grid_width - 1);
+    game->apple_y = rand_int(0, game->grid_height - 1);
+
+    game->input = (input){1, 0, 0};
+
+    game->paused = 0;
+
+    return;
+
+}
+
+void _game_render(game* game, Uint8 r, Uint8 g, Uint8 b, Uint8 a) {
+
+    if (game == NULL) return;
+
+    renderer_clear(&game->renderer, 0, 0, 0, 0);
+
+    struct snake_node* curr = game->snake.head;
+
+    while (curr != NULL) {
+        renderer_drawRect(
+            &game->renderer,
+            curr->x * (game->window_width / game->grid_width), 
+            curr->y  * (game->window_height / game->grid_height), 
+            (game->window_width / game->grid_width) - 2, 
+            (game->window_height / game->grid_height) - 2,
+            r, g, b, a
+        );
+        curr = curr->next;
+    }
+
+    renderer_drawRect(
+        &game->renderer,
+        game->apple_x * (game->window_width / game->grid_width), 
+        game->apple_y * (game->window_height / game->grid_height), 
+        (game->window_width / game->grid_width) - 2, 
+        (game->window_height / game->grid_height) - 2, 
+        255, 0, 0, 0
+    );
+
+    renderer_present(&game->renderer);
+
+}
+
+void _game_paused(game* game) {
+
+    if (game == NULL) return;
+
+    while (game->running && game->paused) {
+        input_read(&game->input, &game->running);
+        if (game->input.restart) {
+            _game_restart(game);
+        }
+        _game_render(game, 50, 50, 50, 0);
+    }
+
+    return;
+
+}
+
+int _game_check_collisions(game* game) {
+
+    if (game == NULL) return 1;
+
+    if (game->snake.head->x >= game->grid_width || game->snake.head->x < 0) {
+        game->paused = 1;
+        return 1;
+    }
+    if (game->snake.head->y >= game->grid_height || game->snake.head->y < 0) {
+        game->paused = 1;
+        return 1;
+    }
+
+    struct snake_node* curr = game->snake.head->next;
+
+    while (curr != NULL) {
+        if (game->snake.head->x == curr->x && game->snake.head->y == curr->y) {
+            game->paused = 1;
+            return 1;
+        }
+        curr = curr->next;
+    }
+
+    // move outside of check_collisions and into _game_update() ?
+    if (game->snake.head->x == game->apple_x && game->snake.head->y == game->apple_y) {
+        snake_move(&game->snake, game->input.dir_x, game->input.dir_y, 1);
+        game->apple_x = rand_int(0, game->grid_width - 1);
+        game->apple_y = rand_int(0, game->grid_height - 1);
+    } else {
+        snake_move(&game->snake, game->input.dir_x, game->input.dir_y, 0);
+    }
+
+    return 0;
+
+}
+
+void _game_update(game* game, Uint32* last_move_time) {
+
+    if (game == NULL || last_move_time == NULL) return;
+
+    Uint32 now = SDL_GetTicks();
+
+    if (game->input.restart) {
+        _game_restart(game);
+        return;
+    }
+
+    if (now - *last_move_time >= game->move_interval) {
+        if (_game_check_collisions(game)) {
+            _game_paused(game);
+        }
+        *last_move_time = now;
+    }
+
+    return;
+
+}
--- src/game.h
+++ src/game.h
@@ -1,8 +1,78 @@
 // game.h
 
-// new branch test
-
 #ifndef GAME_H
 #define GAME_H
+
+#include "input.h"
+#include "random.h"
+#include "renderer.h"
+#include "snake.h"
+
+typedef struct game {
+
+    int running;
+    int paused;
+
+    snake snake;
+    int apple_x, apple_y;
+
+    input input;
+    int move_interval;
+
+    renderer renderer;
+    int window_width, window_height;
+    int grid_width, grid_height;
+
+} game;
+
+/*
+Initializes game engine class, called at the start of main.
+*/
+game game_create(
+    char* window_title,
+    int window_width,
+    int window_height,
+    int grid_width,
+    int grid_height,
+    int move_interval
+);
+
+/*
+Main program loop for running the game engine, called after in main after initializing game.
+*/
+void game_run(game* game);
+
+/*
+Cleans up game engine class, called in main before exiting program.
+*/
+void game_delete(game* game);
+
+/*
+Helper function that re-initializes components of the game engine class, excluding the renderer class.
+*/
+void _game_restart(game* game);
+
+/*
+Helper function for rendering the full current state of the game to the window with custom color for snake.
+*/
+void _game_render(game* game, Uint8 r, Uint8 g, Uint8 b, Uint8 a);
+
+/*
+Helper function that puts game in paused-state loop waiting to be restarted or shut down.
+*/
+void _game_paused(game* game);
+
+/*
+Helper function that checks snake entity's head coordinates with walls, apple, and other snake pieces.
+- Sets paused to 1 and returns 1 if snake collides with itself or a wall.
+- Appends to the snake if it collides with an apple.
+- By default moves the snake in user's desired direction if no collisions occur and returns 0.
+*/
+int _game_check_collisions(game* game);
+
+/*
+Helper function that updates all components of the game engine.
+*/
+void _game_update(game* game, Uint32* last_move_time);
 
 #endif

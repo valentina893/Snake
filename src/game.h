c5058e374ef18a63d29ba9cebe0c3d79b91db456
Valentina <zenit@Valentinas-MacBook-Air.local>
2026-03-06 16:38:49 -0600

    changed pointer layout in function declarations

--- src/game.h
+++ src/game.h
@@ -13,9 +13,10 @@ Game engine class
 */
 typedef struct game {
 
-    renderer renderer;
     snake snake;
-    input input;
+
+    input *input;
+    renderer *renderer;
 
     int running;
     int paused;
@@ -33,7 +34,7 @@ typedef struct game {
 Initializes game engine class, called at the start of main.
 */
 game game_create(
-    char* window_title,
+    char *window_title,
     int window_width,
     int window_height,
     int grid_width,
@@ -44,47 +45,47 @@ game game_create(
 /*
 Main program loop for running the game engine, called after in main after initializing game.
 */
-void game_run(game* game);
+void game_run(game *game);
 
 /*
 Cleans up game engine class, called in main before exiting program.
 */
-void game_delete(game* game);
+void game_delete(game *game);
 
 /*
 Helper function that re-initializes components of the game engine class, excluding the renderer class.
 */
-void _game_restart(game* game);
+void _game_restart(game *game);
 
 /*
 Helper function for rendering current frame when game is running as usual.
 */
-void _game_render_running(game* game);
+void _game_render_running(game *game);
 
 /*
 Helper function for drawing game when user has lost.
 - Snake color is grey, apple stays red.
 */
-void _game_render_lose(game* game);
+void _game_render_lose(game *game);
 
 /*
 Helper function for drawing the game when user has won.
 - Snake fills entire board and drawn with color gold.
 */
-void _game_render_win(game* game);
+void _game_render_win(game *game);
 
 /*
 Helper function that puts game in paused-state loop waiting to be restarted or shut down.
 - lose denotes if it should render the game in a losing state
 - win denotes if it should render the game in the winning state
 */
-void _game_paused(game* game, int lose, int win);
+void _game_paused(game *game, int lose, int win);
 
 /*
 Helper for spawning an apple on a spot where the snake doesn't currently occupy.
 - Process is completed by building arrays of all open x and y spots and choosing random index for each.
 */
-void _game_spawn_apple(game* game);
+void _game_spawn_apple(game *game);
 
 /*
 Helper function that checks snake entity's head coordinates with walls, apple, and other snake pieces.
@@ -92,11 +93,11 @@ Helper function that checks snake entity's head coordinates with walls, apple, a
 - Appends to the snake if it collides with an apple.
 - By default moves the snake in user's desired direction if no collisions occur and returns 0.
 */
-int _game_check_collisions(game* game);
+int _game_check_collisions(game *game);
 
 /*
 Helper function that updates all components of the game engine.
 */
-void _game_update(game* game, Uint32* last_move_time);
+void _game_update(game *game, Uint32 *last_move_time);
 
 #endif

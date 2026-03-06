9ecf54cbe48171dfc43689c427dc8f37345422d2
Valentina <zenit@Valentinas-MacBook-Air.local>
2026-03-06 15:13:56 -0600

    changed snake_node definition to node, changed pointer formatting from 'int* ptr' to 'int *ptr', update header comments to match new struct names

--- src/snake.h
+++ src/snake.h
@@ -4,29 +4,29 @@
 #define SNAKE_H
 
 /*
-One element of the snake entity.
+One element of the snake or apple array.
 - x, y: current position.
-- next: next snake_node furthest from head in direction of travel.
+- next: next node furthest from head in direction of travel.
 */
-typedef struct snake_node {
-    struct snake_node* next;
+typedef struct node {
+    struct node *next;
     int x, y;
-} snake_node;
+} node;
 
 /*
 Snake entity represented by a linked list. Traversed from head -> tail.
-- size: total snake_nodes in snake entity.
-- head: front-most snake_node in direction of traversal.
+- size: total nodes in snake entity.
+- head: front-most node in direction of traversal.
 */
 typedef struct snake {
-    snake_node* head;
+    struct node *head;
     int size;
 } snake;
 
 /*
-Creates a new snake_node.
+Creates a new node.
 */
-snake_node* snake_node_create(int x, int y, struct snake_node* next);
+node *node_create(int x, int y, struct node *next);
 
 /*
 Creates a snake entity of size 1 at specified position.
@@ -34,18 +34,18 @@ Creates a snake entity of size 1 at specified position.
 snake snake_create(int x, int y);
 
 /*
-Moves each snake_node in snake_entity starting from head.
+Moves each node in snake array starting from head.
 */
-void snake_move(struct snake* snake, int dx, int dy, int append);
+void snake_move(snake *snake, int dx, int dy, int append);
 
 /*
 Cleans dynamically allocated memory in snake entity.
 */
-void snake_delete(struct snake* snake);
+void snake_delete(snake *snake);
 
 /*
 Displays the snake entity for debugging purposes.
 */
-void snake_print(struct snake* snake);
+void snake_print(snake *snake);
 
 #endif

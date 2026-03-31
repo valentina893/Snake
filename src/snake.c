914500dbffdfe48f5b43a6d2202fe75b95b6013c
Valentina <zenit@Valentinas-MacBook-Air.local>
2026-03-31 16:00:09 -0500

    created abstract array type for representing snake and apples

--- src/snake.c
+++ src/snake.c
@@ -5,103 +5,39 @@
 
 #include "snake.h"
 
-node *node_create(int x, int y, node *next) {
+array *array_init(int capacity) {
 
-    struct node *node = (struct node*)malloc(sizeof(struct node));
+    array *array = (struct array*)malloc(sizeof(struct array));
+    array->data = (struct node*)malloc(sizeof(struct node));
+    array->size = 0;
+    array->capacity = capacity;
 
-    node->x = x;
-    node->y = y;
-    node->next = next;
-
-    return node;
-
-}
-
-snake *snake_create(int x, int y) {
-
-    snake *snake = (struct snake*)malloc(sizeof(struct snake));
-
-    snake->size = 1;
-    snake->head = node_create(x, y, NULL);
-
-    return snake;
+    return array;
 
 }
 
-void snake_move(snake *snake, int dx, int dy, int append) {
+int array_search(array *array, node n) {
 
-    if (snake == NULL || snake->head == NULL) return;
-
-    int temp_x, temp_y;
-    int prev_x = snake->head->x, prev_y = snake->head->y;
-
-    snake->head->x += dx, snake->head->y += dy;
-
-    node *curr = snake->head;
-
-    while (curr->next != NULL) {
-
-        // Move to next node
-        curr = curr->next;
-        
-        // Save current node's coordinates
-        temp_x = curr->x;
-        temp_y = curr->y;
-
-        // Update current node to previous position
-        curr->x = prev_x;
-        curr->y = prev_y;
-
-        // Shift the "previous" values forward
-        prev_x = temp_x;
-        prev_y = temp_y;
-        
+    if (array != NULL && array->data != NULL) {
+        for (int i = 0; i < array->capacity; i++) {
+            if (array->data[i].x == n.x && array->data[i].y == n.y) {
+                return 1;
+            }
+        }
     }
 
-    if (append) {
-        curr->next = node_create(prev_x, prev_y, NULL);
-        snake->size++;
-    }
-
-    return;
+    return 0;
 
 }
 
-void snake_delete(snake *snake) {
+void array_delete(array *array) {
 
-    if (snake == NULL) return;
-
-    node *curr = snake->head;
-
-    while (curr != NULL) {
-        node *temp = curr->next;
-        free(curr);
-        curr = temp;
-    }
-
-    snake->head = NULL;
-    snake->size = 0;
-
-    free(snake);
-
-    return;
-
-}
-/*
-void apple_arr_create(apple_arr *apple_arr, int size) {
-
-    apple_arr->data = (struct node*)malloc(sizeof(struct node) * size);
-    apple_arr->size = size;
-
-    for (int i = 0; i < size; i++) {
-        //apple_arr->data[i] = node_create()
+    if (array != NULL) {
+        if (array->data != NULL) {
+            free(array->data);
+            array->size, array->capacity = 0;
+        }
+        free(array);
     }
 
 }
-
-void apple_arr_delete(apple_arr *apple_arr) {
-    if (apple_arr != NULL) {
-        free(apple_arr->data);
-    }
-}
-*/
--- src/snake.h
+++ src/snake.h
@@ -6,59 +6,33 @@
 /*
 One element of the snake or apple array.
 - x, y: current position.
-- next: next node furthest from head in direction of travel.
 */
 typedef struct node {
-    struct node *next;
     int x, y;
 } node;
 
 /*
-Snake entity represented by a linked list. Traversed from head -> tail.
-- size: total nodes in snake entity.
-- head: front-most node in direction of traversal.
+Abstract array type for representing the snake or apples.
 */
-typedef struct snake {
-    struct node *head;
+typedef struct array {
+    node *data;
     int size;
-} snake;
+    int capacity;
+} array;
 
 /*
-Array that holds one or more apples on screen.
+Initializes abstract array type with maxiumum capacity;
 */
-typedef struct apple_arr {
-    struct node *data;
-    int size;
-} apple_arr;
+array *array_init(int capacity);
 
 /*
-Creates a new node.
+Searches array for node n, returns 1 if found, else returns 0.
 */
-node *node_create(int x, int y, struct node *next);
+int array_search(array *array, node n);
 
 /*
-Creates a snake entity of size 1 at specified position.
+Cleans up abstract array type.
 */
-snake *snake_create(int x, int y);
-
-/*
-Moves each node in snake array starting from head.
-*/
-void snake_move(snake *snake, int dx, int dy, int append);
-
-/*
-Cleans dynamically allocated memory in snake entity.
-*/
-void snake_delete(snake *snake);
-
-/*
-Initializes apple array with each node containing random coords.
-*/
-void apple_arr_create(apple_arr *apple_arr, int size);
-
-/*
-Frees dynamically allocated apple array.
-*/
-void apple_arr_delete(apple_arr *apple_arr);
+void array_delete(array *array);
 
 #endif
